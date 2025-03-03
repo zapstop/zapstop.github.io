699292b77d080ba7cf56df268e3e85927c2b919b
zapstop <zapstops@gmail.com>
2025-03-03 11:50:19 +0800

    Site updated: 2025-03-03 11:50:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/4a17b156.html"];function toRandomPost(){
+var posts=["posts/a1fb3e72.html","posts/4a17b156.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
